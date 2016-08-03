c736e4a0eb40323265b3e8959b90678ba7ab82a2
Paul Meskers <paulmeskers@gmail.com>
2016-08-03 12:28:31 -0400

    wipppppppppppppppppppppppppppp

---
+++ src/ts/reducers.ts
@@ -0,0 +1,11 @@
+import { SELECT_COLOR } from './actions';
+
+let defaultColor = new Color(40, 110, 170);
+
+function selectedColor(state = defaultColor, action) {
+  switch (action.type) {
+      
+    default:
+      return state;
+  }
+}
